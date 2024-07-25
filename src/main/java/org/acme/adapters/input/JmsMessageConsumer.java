95463f0a23ecc5a530664b42bdd44c57cfc9c7da
j0e sp3cim3n <joe.specimen@gmail.com>
2024-07-25 18:47:33 +0200

    Removed commented code.

--- src/main/java/org/acme/adapters/input/JmsMessageConsumer.java
+++ src/main/java/org/acme/adapters/input/JmsMessageConsumer.java
@@ -37,8 +37,6 @@ public class JmsMessageConsumer {
    @ConfigProperty(name = "my.edu.queue.name", defaultValue = "my.edu.queue")
    String queueName;
 
-   // private final ExecutorService scheduler = Executors.newSingleThreadExecutor();
-
    private AtomicInteger counter = new AtomicInteger(0);
 
 
@@ -109,112 +107,5 @@ public class JmsMessageConsumer {
          log.severe("Error sending message to DLQ: %s".formatted(e.getMessage()));
       }
    }
-
-
-   /*
-
-   2nd Version
-   void onStart(@Observes StartupEvent ev) {
-      context = connectionFactory.createContext(JMSContext.SESSION_TRANSACTED);
-      JMSConsumer consumer = context.createConsumer(context.createQueue(queueName));
-      consumer.setMessageListener(this::onMessage);
-   }
-
-   void onStop(@Observes ShutdownEvent ev) {
-      // scheduler.shutdown();
-   }
-
-   private void onMessage(Message message) {
-      try {
-         String text = message.getBody(String.class);
-         processMessage(text);
-         message.acknowledge();
-      } catch (Exception e) {
-         context.rollback();
-         log.severe("Error processing message: %s".formatted(e.getMessage()));
-         throw new RuntimeException("Failed to process message", e);
-      }
-   }
-
-   private void processMessage(String text) {
-      // Your business logic here
-      counter.incrementAndGet();
-      if (counter.get() % 5 == 0) {
-         throw new RuntimeException("Error in business logic");
-      }
-      log.info("Processed message: " + text);
-   }
-
-    */
-
-   /*
-
-   1st Version
-   @Override
-   public void run() {
-      while (true) {
-         try (JMSContext context = connectionFactory.createContext(JMSContext.CLIENT_ACKNOWLEDGE);
-              JMSConsumer consumer = context.createConsumer(context.createQueue(queueName))) {
-            log.info("Waiting to receive!");
-            Message message = consumer.receive();
-            if (message != null) {
-               processMessage(message, context);
-            }
-         }
-      }
-   }
-
-   private void processMessage(Message message, JMSContext context) {
-      try {
-         if (message instanceof TextMessage) {
-            int ii = counter.incrementAndGet();
-            String text = ((TextMessage) message).getText();
-            if (ii % 5 == 0) {
-
-               log.info("Send msg to DLQ[%d]".formatted(counter.get()));
-               // context.recover();
-
-               String dlqQueueName = queueName + ".dlq";
-               try(JMSContext producerContext = connectionFactory.createContext(JMSContext.AUTO_ACKNOWLEDGE)) {
-                  JMSProducer producer =
-                      producerContext.createProducer().send(producerContext.createQueue(dlqQueueName),
-                                                            message);
-               }
-
-               return;
-            }
-            log.info("Acknowledge msg[%d]".formatted(counter.get()));
-            // message.acknowledge();
-         }
-      } catch (Exception e) {
-         log.severe(e.getMessage());
-      }
-   }
-    */
-
-   /*
-   private void sendToDLQ(JMSContext context, Queue dlq, Message originalMessage) {
-      try {
-         Message dlqMessage = context.createTextMessage(((TextMessage) originalMessage).getText());
-         // Copy properties and headers from original message
-         for (String propertyName : Collections.list(originalMessage.getPropertyNames())) {
-            dlqMessage.setObjectProperty(propertyName, originalMessage.getObjectProperty(propertyName));
-         }
-         dlqMessage.setJMSCorrelationID(originalMessage.getJMSMessageID());
-         dlqMessage.setStringProperty("OriginalQueue", queueName);
-         dlqMessage.setStringProperty("ErrorMessage", "Failed to process in business logic");
-
-         JMSProducer producer = context.createProducer();
-         producer.send(dlq, dlqMessage);
-         log.info("Message sent to DLQ");
-
-         // Acknowledge the original message after sending to DLQ
-         originalMessage.acknowledge();
-      } catch (JMSException e) {
-         log.errorf("Error sending message to DLQ: %s", e.getMessage());
-      }
-   }
-
-    */
 
 }
